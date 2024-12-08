ab8586c324467e6806854766008398b7c2f384e6
RJ <rjparaiso369@gmail.com>
2024-12-08 23:52:17 +0000

    feat: Added more commands to the bot

--- cogs/fun.py
+++ cogs/fun.py
@@ -22,3 +22,14 @@ class FunCom(commands.Cog):
             )
         embed.set_footer(text=f"See r!help quote for more details")
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def kiss(self, ctx, arg=""):
+        gifGif = random.choice(KISS_LIST)
+
+        if arg == "":
+            await ctx.send("You can't really kiss *nothing*, you idiot. What, are you a loner?")
+        
+        else:
+            await ctx.send(embed=gifEmbed("Seems to be a little romance here?", gifGif, f"{ctx.author.mention} kisses {arg}", "Love is in the air tonight..."))
+    
